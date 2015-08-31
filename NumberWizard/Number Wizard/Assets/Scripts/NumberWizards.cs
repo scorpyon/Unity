46cc99d9441f1ae2606b0dfd5a534ec13214b8d5
Danny Jeffery <daniel.p.jeffery@gmail.com>
2015-08-31 10:30:45 +0100

    More Files

--- NumberWizard/Number Wizard/Assets/Scripts/NumberWizards.cs
+++ NumberWizard/Number Wizard/Assets/Scripts/NumberWizards.cs
@@ -3,28 +3,46 @@ using System.Collections;
 
 public class NumberWizards : MonoBehaviour {
 
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
+	
 	// Use this for initialization
 	void Start () {
+		StartGame();
+	}
+	
+	void StartGame(){
+		max++;
 		print ("Welcome to Number Wizard!");
 		print ("Pick a number in your head, but don't tell me what it is!");
-		
-		int max = 1000;
-		int min = 1;
 		
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
 		
-		print ("Is the number higher or lower than 500?");
-		print ("Arrow = Higher, Down Arrow = Lower and Right Arrow if this is your number!");
+		print ("Is the number higher or lower than "+ guess + "?");
+		print ("Arrow = Higher, Down Arrow = Lower, and Enter if this is your number!");
+	}
+	
+	void NextGuess(){
+		guess = (min + max) / 2;
+		print("Higher or Lower than " + guess + "?");
+		print ("Arrow = Higher, Down Arrow = Lower, and Enter if this is your number!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.UpArrow)){
-			print ("Up Arrow pressed.");
+			min = guess;
+			NextGuess();
 		}
-		if(Input.GetKeyDown(KeyCode.DownArrow)){
-			print ("Down Arrow pressed.");
+		else if(Input.GetKeyDown(KeyCode.DownArrow)){
+			max = guess;
+			NextGuess();
+		}
+		else if(Input.GetKeyDown(KeyCode.Return)){
+			print ("I won!");
+			guess = 500;
 		}
 	}
 }
