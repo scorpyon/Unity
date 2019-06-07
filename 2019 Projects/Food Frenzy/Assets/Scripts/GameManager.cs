5cd349e2cc11bf0c23db1a0e4bf60ac94e3e4ebe
Danny Jeffery <daniel.jeffery@globalgraphics.com>
2019-06-07 16:32:31 +0100

    Created Game Manager

---
+++ 2019 Projects/Food Frenzy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int score;
+    public int gameTimeRemaining;
+
+    void Awake()
+    {
+        MakeSingleton();
+    }
+
+    private void MakeSingleton()
+    {
+        // This guarantees that this is the correctly used Manager (and not a new copy)
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Do Nothing
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Do Nothing
+    }
+}
