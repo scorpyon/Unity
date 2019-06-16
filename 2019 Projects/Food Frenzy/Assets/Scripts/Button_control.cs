766078a90ea0c1caf417ec279481640c7555c7c7
Danny Jeffery <daniel.p.jeffery@gmail.com>
2019-06-16 18:01:53 +0100

    Added Foodomatic and button

---
+++ 2019 Projects/Food Frenzy/Assets/Scripts/Button_control.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_control : MonoBehaviour
+{
+    private Vector3 buttonPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > buttonPos.x + 0.04f)
+            transform.position = new Vector3(buttonPos.x + 0.04f, transform.position.y, transform.position.z);
+        else if (transform.position.x < buttonPos.x)
+            transform.position = new Vector3(buttonPos.x, transform.position.y, transform.position.z);
+    }
+}
