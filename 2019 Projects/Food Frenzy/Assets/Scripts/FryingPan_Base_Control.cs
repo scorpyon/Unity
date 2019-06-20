6d3557adaf33e805f7f50634819114baf9d34ca2
Danny Jeffery <daniel.p.jeffery@gmail.com>
2019-06-20 19:13:19 +0100

    Frying Pan stuff

---
+++ 2019 Projects/Food Frenzy/Assets/Scripts/FryingPan_Base_Control.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FryingPan_Base_Control : MonoBehaviour
+{
+    private List<GameObject> ColliderObjects = new List<GameObject>();
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //other.transform.position = transform.position + transform.TransformDirection(0, 0, 1);
+        foreach (var colliderObject in ColliderObjects)
+        {
+            if (colliderObject.tag == "burger_raw"
+                || colliderObject.tag == "burger_cooked"
+                || colliderObject.tag == "burger_burnt")
+            {
+                colliderObject.transform.parent = transform;
+                colliderObject.transform.localPosition = new Vector3(0,1,0);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        ColliderObjects.Add(other.gameObject);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (ColliderObjects.Contains(other.gameObject))
+        {
+            ColliderObjects.Remove(other.gameObject);
+        }
+    }
+}
